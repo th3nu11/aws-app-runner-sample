0697e90f5e3ef2a3aee9bf959ad226be07c9f61b
carlo.ballabio <ballabio@intre.it>
2024-05-03 17:00:57 +0200

    Add apprunner configuration file for hello-world project

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World, this is my first app runner application!');
