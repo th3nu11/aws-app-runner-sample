6e14c9a4a923056871ace09653ac9ca834433809
carlo.ballabio <ballabio@intre.it>
2024-05-03 16:56:10 +0200

    Add hello world project

---
+++ hello-world/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World, this is my first app runner application!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
